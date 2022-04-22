3b2a394aa9d844167377dad1e691247b23cd7fdc
otienosteve <steveotieno701@gmail.com>
2022-04-22 16:35:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -8,9 +8,18 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
+  I am going to take a string convert it into an array and reverse the array after trhat I am going to convert that array onto a string
+
 */
 
 // You can run `node index.js` to view these console logs
+
+
+
+function isPalindrome(str){
+var palindrome=str.split('').reverse().join('')
+return palindrome===str
+}
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
